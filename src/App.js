f6ad886c9e20cd7c385d7dbb306e69cb42a4c0cc
Ian V <ianenrico1@gmail.com>
2021-11-21 14:27:37 -0500

    Async and formatting

--- src/App.js
+++ src/App.js
@@ -3,50 +3,55 @@ import api from "./api.js";
 import { useState, useEffect } from "react";
 
 function App() {
+  //States
+  //State to store data from API
   const [responseDatas, setResponseDatas] = useState("");
+  //State to display isLoading before axios returns data from API
   const [loading, setLoading] = useState(true);
+  //State to display an error message if data isn't retrieved
   const [error, setError] = useState(null);
 
+  //Transform date timestamp from API to a weekday
   function getWeekday(timestamp) {
     const date = new Date(timestamp * 1000);
     const day = date.toDateString().split(" ")[0];
     return day;
   }
 
+  //Round temp to nearest degree C
   function fixTemp(temp) {
     return Math.round(temp);
   }
 
+  //useEffect hook to call fetchData
   useEffect(() => {
     fetchData();
   }, []);
 
+  //Async function using axios to get the data from the OpenWeather API
   async function fetchData() {
-    api
-      .getData()
+    try {
+      const data = await api.getData();
+      console.log(data.data);
+      return setResponseDatas(data.data);
+    } catch (erorr) {
+      console.log("Error Fetching Data", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
 
-      .then((response) => {
-        setResponseDatas(response.data);
-        console.log(response.data);
-        // console.log("Response Data", response.data);
-      })
-      .catch((error) => {
-        console.log("Error Fetching Data", error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
   }
   if (loading) return "Loading...";
   if (error) return "Error!";
   return (
-    <div  className="h-screen flex items-center justify-center text-center text-gray-500 font-bold  ">
+    <div className="h-screen flex items-center justify-center text-center text-gray-500 font-bold  ">
       {responseDatas.map((data) => (
         <div key={data.sunrise} id="Card-wrapper">
           <div id="card" className="w-24 mx-2">
             <h3 id="day">{getWeekday(data.dt)}</h3>
             <img
+              alt={data.weather[0].description}
               className="mx-auto"
               src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             />
--- src/api.js
+++ src/api.js
@@ -7,17 +7,16 @@ const instance = axios.create({
 const KEY = process.env.REACT_APP_API_KEY;
 
 export default {
-    getData: () =>
+  getData: () =>
     instance({
-        "method": "get",
-        "url": `onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly&units=metric&appid=${KEY}`,
-        transformResponse: [function (data) {
-            const json = JSON.parse(data)
-            const daily = json.daily.slice(0,5)   
-            return daily
-        }]
-    })
-}
-
-
-
+      method: "get",
+      url: `onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly&units=metric&appid=${KEY}`,
+      transformResponse: [
+        function (data) {
+          const json = JSON.parse(data);
+          const daily = json.daily.slice(0, 5);
+          return daily;
+        },
+      ],
+    }),
+};
